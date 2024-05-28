3c321e066629dcd880c52ba208687f3a42d90875
Killian Kayla <killiankayla.e@gmail.com>
2024-05-28 15:37:49 -0400

    added toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@
 // use to add components to all pages or manage authentication 
 import "@/styles/globals.css";
 import Navbar from "@/components/NavBar";
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>   
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Link from 'next/link';
 import Loader from '@/components/Loader';
+import toast from 'react-hot-toast';
 
 // Link can just be an href string
 // Or can be explicit for the path and the query to perform
@@ -10,7 +11,9 @@ import Loader from '@/components/Loader';
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
       Homepage
     </div>
   );
